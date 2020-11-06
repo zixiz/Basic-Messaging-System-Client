ddfb667c7695c0109eb7e5462ec30fe66327a2be
zixiz <gshachargilad@gmail.com>
2020-11-06 14:01:28 +0200

    finished

--- src/api/messagesUrl.js
+++ src/api/messagesUrl.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 import {PRODUCTION_SERVER,DEVELOPMENT_SERVER} from './Config';
 
 export default axios.create({
-    baseURL:DEVELOPMENT_SERVER.MESSAGES_URL
+    baseURL:PRODUCTION_SERVER.MESSAGES_URL
 })
