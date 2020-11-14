b2e0a9a875b9b089ee1b6e2369dfad3d0e72cdb2
zixiz <gshachargilad@gmail.com>
2020-11-14 12:19:36 +0200

    fix invalid token

--- src/actions/index.js
+++ src/actions/index.js
@@ -79,7 +79,7 @@ export const signUp = (formValues) => async (dispatch) =>{
 
 
 export const signOut = () => async (dispatch) =>{
-    localStorage.removeItem("user");
+    localStorage.clear()
     dispatch({type:SIGN_OUT,payload:{}});
     history.push(PATH.SIGN_IN);
 }
@@ -96,12 +96,17 @@ export const fetachReciveMessages = () => async (dispatch)=> {
     try{
         dispatch({type:ACTIVE_SCREENLOADER, payload:{}})
         const response = await messagesUrl.get("/message",authHeader());
-        if(response.data){
+        if(response.data.success){
             dispatch({type: FETCH_RECIVEMESSAGES, payload:response.data.response})
             if(response.data.response.length === 0) dispatch(emptyMessages())
             setTimeout(()=>{
                 dispatch({type:DISABLE_SCREENLOADER, payload:{}})
             },200)
+        }else{
+            setTimeout(()=>{
+                dispatch({type:DISABLE_SCREENLOADER, payload:{}})
+            },200)
+            if(typeof(response.data.isLoggedIn) != "undefined" && response.data.isLoggedIn === false) return dispatch(signOut())
         }
     }catch(error){
         dispatch({type:DISABLE_SCREENLOADER, payload:{}});
@@ -116,13 +121,18 @@ export const fetachSentMessages = () => async (dispatch)=> {
         dispatch({type:ACTIVE_SCREENLOADER, payload:{}})
         const response = await messagesUrl.get("/message/?filter=sent",authHeader());
         
-        if(response.data){
+        if(response.data.success){
             dispatch({type: FETCH_SENTMESSAGES, payload:response.data.response})
             if(response.data.response.length === 0) dispatch(emptyMessages())
             setTimeout(()=>{
                 dispatch({type:DISABLE_SCREENLOADER, payload:{}})
             },200)
-        }   
+        }else{
+            setTimeout(()=>{
+                dispatch({type:DISABLE_SCREENLOADER, payload:{}})
+            },200)
+            if(typeof(response.data.isLoggedIn) != "undefined" && response.data.isLoggedIn === false) return dispatch(signOut())
+        }
     }catch(error){
         dispatch({type:DISABLE_SCREENLOADER, payload:{}});
         dispatch(serverErrorActionCreator(SERVER_ERROR_SHOW,error.message));
@@ -142,13 +152,15 @@ export const fetchMessage = (id) => async (dispatch)=>{
     try{
         dispatch({type:ACTIVE_SCREENLOADER, payload:{}})
         const response = await messagesUrl.get(`/message/${id}`,authHeader());
-        if(response.data.success){
-            dispatch({type:FETCH_MESSAGE,payload:{message:response.data.message,messageUserData:response.data.messageUserData}})
+        const {data} = response;
+        if(data.success){
+            dispatch({type:FETCH_MESSAGE,payload:{message:data.message,messageUserData:data.messageUserData}})
             setTimeout(()=>{
                 dispatch({type:DISABLE_SCREENLOADER, payload:{}})
             },200)
         }else{
-            dispatch({type:FAILEDSHOW_MESSAGE,payload:response.data})
+            if(typeof(data.isLoggedIn) != "undefined" && data.isLoggedIn === false) return dispatch(signOut())
+            dispatch({type:FAILEDSHOW_MESSAGE,payload:data})
             setTimeout(()=>{
                 dispatch({type:DISABLE_SCREENLOADER, payload:{}})
             },200)
@@ -167,16 +179,17 @@ export const fetchUsers = () => async (dispatch) =>{
     try{
         dispatch({type:ACTIVE_SCREENLOADER, payload:{}})
         const response = await messagesUrl.get('/users',authHeader());
-        if(response.data.success){
-            dispatch({type:FETCH_USERS,payload:response.data.response});
+        const {data} = response;
+        if(data.success){
+            dispatch({type:FETCH_USERS,payload:data.response});
             setTimeout(()=>{
                 dispatch({type:DISABLE_SCREENLOADER, payload:{}})
             },100)
         }else{
-            alert(response.data.error)
             setTimeout(()=>{
                 dispatch({type:DISABLE_SCREENLOADER, payload:{}})
             },100)
+            if(typeof(data.isLoggedIn) != "undefined" && data.isLoggedIn === false) return dispatch(signOut())
         }
     }catch(error){
         dispatch({type:DISABLE_SCREENLOADER, payload:{}});
